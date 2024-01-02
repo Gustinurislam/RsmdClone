5bcba1a707955f839f11185274f08e5b27d46719
Gusti Nur Islam <gustinurislam@gmail.com>
2024-01-02 10:13:59 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar"
+
+const App = () => {
+
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import { PhoneIcon, PlusIcon } from '@heroicons/react/24/solid'
+import { EnvelopeIcon, Bars3BottomRightIcon } from '@heroicons/react/24/outline'
+
+export default function Navbar() {
+    return (
+        <>
+            <nav className="flex">
+                <img src="https://rsmedikadramaga.com/assets/images/logo.png" className='py-8 pl-24 pr-8 cursor-pointer' alt="logo" />
+                <div className="-skew-x-[24deg] border-r border-[#2CB4AC]" />
+                <div className='flex flex-col flex-1 border-l border-[#2CB4AC] -skew-x-[24deg] font-semibold'>
+                    <ul className='px-10 flex justify-around items-center bg-gradient-to-r from-[#2CB4AC] to-[#439CC4] text-white py-3'>
+                        <li className='flex items-center'><span><PhoneIcon className='w-6 h-6 pr-2' /></span>Telp : (0251) 8308900 / Whatsapp : 081319310610</li>
+                        <li className='flex items-center'> <span><EnvelopeIcon className='w-6 h-6 pr-2' /></span>Email : rsmedikabogor@gmail.com
+                        </li>
+                        <li><span><Bars3BottomRightIcon className='w-10 h-10 text-white cursor-pointer' /></span></li>
+                    </ul>
+                    <ul className='flex items-center justify-around py-5 px-14'>
+                        <li className='cursor-pointer '>Beranda</li>
+                        <li className='cursor-pointer flex items-center'>Tentang Kami <span><PlusIcon className='w-5 h-5 pl-1 text-[#2CB4AC]' /></span></li>
+                        <li className='cursor-pointer flex items-center'>Layanan <span><PlusIcon className='w-5 h-5 pl-1 text-[#2CB4AC]' /></span></li>
+                        <li className='cursor-pointer flex items-center'>Jadwal Poliklinik <span><PlusIcon className='w-5 h-5 pl-1 text-[#2CB4AC]' /></span></li>
+                        <li className='cursor-pointer flex items-center'>Informasi <span><PlusIcon className='w-5 h-5 pl-1 text-[#2CB4AC]' /></span></li>
+                        <li className='cursor-pointer '>Kontak </li>
+                        <li className='cursor-pointer '>Download Aplikasi </li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    )
+}
