67d070aaf1dc1c2a1b7f246f6dd538a57bbfc5f7
Gusti Nur Islam <gustinurislam@gmail.com>
2024-01-03 23:45:50 +0700

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,8 +9,8 @@ export default function Navbar() {
                 <div className="-skew-x-[24deg] border-r border-[#2CB4AC]" />
                 <div className='flex flex-col flex-1 border-l border-[#2CB4AC] -skew-x-[24deg] font-semibold'>
                     <ul className='px-10 flex justify-around items-center bg-gradient-to-r from-[#2CB4AC] to-[#439CC4] text-white py-3'>
-                        <li className='flex items-center'><span><PhoneIcon className='w-6 h-6 pr-2' /></span>Telp : (0251) 8308900 / Whatsapp : 081319310610</li>
-                        <li className='flex items-center'> <span><EnvelopeIcon className='w-6 h-6 pr-2' /></span>Email : rsmedikabogor@gmail.com
+                        <li className='flex items-center'><span><PhoneIcon className='w-6 h-6 pr-2' /></span>Telp : (0101) 8308900 / Whatsapp : 01010101010</li>
+                        <li className='flex items-center'> <span><EnvelopeIcon className='w-6 h-6 pr-2' /></span>Email : rsmdclone@gmail.com
                         </li>
                         <li><span><Bars3BottomRightIcon className='w-10 h-10 text-white cursor-pointer' /></span></li>
                     </ul>
