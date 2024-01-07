9ce19f502c63c6210140c86c3d94ff6ca3321f14
Gusti Nur Islam <gustinurislam@gmail.com>
2024-01-08 00:31:38 +0700

    add framer motion

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import 'swiper/css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay } from 'swiper/modules';
+import { motion } from "framer-motion"
 
 export default function Hero() {
     return (
@@ -9,7 +10,7 @@ export default function Hero() {
                 spaceBetween={30}
                 centeredSlides={true}
                 autoplay={{
-                    delay: 7000,
+                    delay: 8000,
                     disableOnInteraction: false,
                 }}
 
@@ -20,9 +21,9 @@ export default function Hero() {
                 <SwiperSlide>
                     <div className="flex items-center justify-between px-16 bg-[url('https://rsmedikadramaga.com/assets/images/main-slider/1.jpg')] bg-no-repeat bg-cover h-screen">
                         <div className="text-white space-y-10">
-                            <h3 className="text-4xl underline italic font-semibold">Pelayanan</h3>
-                            <h2 className="text-5xl font-bold">RS Medika Dramaga</h2>
-                            <h3 className="text-3xl font-bold">TERBAIK & TERPERCAYA</h3>
+                            <motion.div initial={{ opacity: 0 }} animate={{ y: -30, opacity: 1 }} transition={{ duration: 1 }} exit={{opacity: 0}} className="text-4xl underline italic font-semibold">Pelayanan</motion.div>
+                            <motion.div initial={{ opacity: 0 }} animate={{ y: -30, opacity: 1 }} transition={{ duration: 1.5 }} exit={{opacity: 0}} className="text-5xl font-bold">RS Medika Dramaga</motion.div>
+                            <motion.div initial={{ opacity: 0 }} animate={{ y: -30, opacity: 1 }} transition={{ duration: 2 }} exit={{opacity: 0}} className="text-3xl font-bold">TERBAIK & TERPERCAYA</motion.div>
                         </div>
 
                         <div>
