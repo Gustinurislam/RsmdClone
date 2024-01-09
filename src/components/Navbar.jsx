f74ad1501f4c07a6f0dbdc73a0f35bac020f29ed
Gusti Nur Islam <gustinurislam@gmail.com>
2024-01-09 23:41:47 +0700

    update img

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,11 @@
 import { PhoneIcon, PlusIcon } from '@heroicons/react/24/solid'
 import { EnvelopeIcon, Bars3BottomRightIcon } from '@heroicons/react/24/outline'
-import logo from '../../data/logo.json'
 
 export default function Navbar() {
     return (
         <>
             <nav className="flex overflow-hidden">
-                {logo.map(({ uid, image }) => (
-                    <img key={uid} src={image} className='py-8 pl-24 pr-8 cursor-pointer' alt="logo" />
-                ))}
+                    <img src='/img/logo.png' className='py-8 pl-24 pr-8 cursor-pointer' alt="logo" />
                 <div className="-skew-x-[24deg] border-r border-[#2CB4AC]" />
                 <div className='flex flex-col flex-1 border-l border-[#2CB4AC] -skew-x-[24deg] font-semibold'>
                     <ul className='px-10 flex justify-around items-center bg-gradient-to-r from-[#2CB4AC] to-[#439CC4] text-white py-1'>
